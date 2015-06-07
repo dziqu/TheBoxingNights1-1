da685aeafc8676a822a71932d8d352b94e113d82
dziku <fenix_91@go2.pl>
2015-06-07 12:15:43 +0200

    Klasa Start - dodałem inicjację ustawień początkowych

--- THeBoxingNights1-1/src/pl/theboxingnights/app/start/Start.java
+++ THeBoxingNights1-1/src/pl/theboxingnights/app/start/Start.java
@@ -1,19 +1,33 @@
 package pl.theboxingnights.app.start;
 
 import com.jme3.app.SimpleApplication;
+import pl.theboxingnights.app.settings.Settings;
 
 /**
  * Created by filip / 07.06.15 / 06:55
  */
 public class Start extends SimpleApplication {
 
+    public Start() {
+        setSettings();
+    }
+
     public static void main (String [] args) {
         Start app = new Start();
         app.start();
+    }
+
+    private void setSettings() {
+        Settings settings = new Settings(this);
+        settings.load();
+        settings.getAll();
+        settings.setAll();
+        settings.save();
     }
 
     @Override
     public void simpleInitApp() {
 
     }
+
 }
