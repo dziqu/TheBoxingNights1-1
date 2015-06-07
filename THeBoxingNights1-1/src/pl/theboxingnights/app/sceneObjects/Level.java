ae735b8a1f96dbddcf1567522f33a4d6af0f7a0a
dziku <fenix_91@go2.pl>
2015-06-07 13:59:47 +0200

    Dodałem interfejs Level

---
+++ THeBoxingNights1-1/src/pl/theboxingnights/app/sceneObjects/Level.java
@@ -0,0 +1,16 @@
+package pl.theboxingnights.app.sceneObjects;
+
+/**
+ * Created by filip / 07.06.15 / 13:50
+ */
+public interface Level {
+
+    float getMaxStrength();
+
+    float getMaxSpeed();
+
+    float getMaxLife();
+
+    float getMaxStamina();
+
+}
