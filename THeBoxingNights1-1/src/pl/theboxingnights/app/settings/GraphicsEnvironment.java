43d4bf544f67c86f5529b67549b63e1c49ca5754
dziku <fenix_91@go2.pl>
2015-06-07 12:14:32 +0200

    Klasa GraphicsEnvironment - refaktoryzacja

--- THeBoxingNights1-1/src/pl/theboxingnights/app/settings/GraphicsEnvironment.java
+++ THeBoxingNights1-1/src/pl/theboxingnights/app/settings/GraphicsEnvironment.java
@@ -1,7 +1,6 @@
 package pl.theboxingnights.app.settings;
 
 import java.awt.*;
-import java.util.List;
 
 /**
  * Created by filip / 07.06.15 / 11:23
@@ -13,24 +12,53 @@ public final class GraphicsEnvironment {
     private DisplayMode[] displayModes = null;
 
     public GraphicsEnvironment() {
-        localGraphicsEnvironment = java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment();
-        graphicsDevice = localGraphicsEnvironment.getDefaultScreenDevice();
-        displayModes = graphicsDevice.getDisplayModes();
+        setLocalGraphicsEnvironment(java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment());
+        setGraphicsDevice(getLocalGraphicsEnvironment().getDefaultScreenDevice());
+        setDisplayModes(getGraphicsDevice().getDisplayModes());
     }
 
     public int getMaxHeight() {
-        return displayModes[displayModes.length - 1].getHeight();
+        return getDisplayModes()[getDisplayModeNumber()].getHeight();
     }
 
     public int getMaxWidth() {
-        return displayModes[displayModes.length - 1].getWidth();
+        return getDisplayModes()[getDisplayModeNumber()].getWidth();
     }
 
     public int getMaxRefreshRate() {
-        return displayModes[displayModes.length - 1].getRefreshRate();
+        return getDisplayModes()[getDisplayModeNumber()].getRefreshRate();
     }
 
     public int getMaxBitDepth() {
-        return displayModes[displayModes.length - 1].getBitDepth();
+        return getDisplayModes()[getDisplayModeNumber()].getBitDepth();
     }
+
+    public java.awt.GraphicsEnvironment getLocalGraphicsEnvironment() {
+        return localGraphicsEnvironment;
+    }
+
+    public void setLocalGraphicsEnvironment(java.awt.GraphicsEnvironment localGraphicsEnvironment) {
+        this.localGraphicsEnvironment = localGraphicsEnvironment;
+    }
+
+    public GraphicsDevice getGraphicsDevice() {
+        return graphicsDevice;
+    }
+
+    public void setGraphicsDevice(GraphicsDevice graphicsDevice) {
+        this.graphicsDevice = graphicsDevice;
+    }
+
+    public DisplayMode[] getDisplayModes() {
+        return displayModes;
+    }
+
+    public void setDisplayModes(DisplayMode[] displayModes) {
+        this.displayModes = displayModes;
+    }
+
+    public int getDisplayModeNumber() {
+        return 0;
+    }
+
 }
