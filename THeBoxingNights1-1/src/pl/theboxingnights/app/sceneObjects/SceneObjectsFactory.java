460d25f476a41790ea6205205163b7d53eaebfeb
dziku <fenix_91@go2.pl>
2015-06-07 14:00:16 +0200

    Dodałem klasę SceneObjectsFactory

--- THeBoxingNights1-1/src/pl/theboxingnights/app/sceneObjects/SceneObjectsFactory.java
+++ THeBoxingNights1-1/src/pl/theboxingnights/app/sceneObjects/SceneObjectsFactory.java
@@ -1,7 +1,12 @@
-package pl.theboxingnights.app.players;
+package pl.theboxingnights.app.sceneObjects;
 
 /**
  * Created by filip / 07.06.15 / 12:16
  */
 public class SceneObjectsFactory {
+
+
+
+
+
 }
