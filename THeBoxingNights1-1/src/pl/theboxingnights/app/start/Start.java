c9a55aaf99a384bc3560851203be03246d5b5ee2
dziku <fenix_91@go2.pl>
2015-06-07 07:00:47 +0200

    Dodałem metodę main

--- THeBoxingNights1-1/src/pl/theboxingnights/app/start/Start.java
+++ THeBoxingNights1-1/src/pl/theboxingnights/app/start/Start.java
@@ -1,7 +1,19 @@
 package pl.theboxingnights.app.start;
 
+import com.jme3.app.SimpleApplication;
+
 /**
  * Created by filip / 07.06.15 / 06:55
  */
-public class Start {
+public class Start extends SimpleApplication {
+
+    public static void main (String [] args) {
+        Start app = new Start();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+
+    }
 }
