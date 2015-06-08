9a6b36ed4e7fc4f0f47798a43be11cfdfc7204fa
dziku <fenix_91@go2.pl>
2015-06-08 04:07:45 +0200

    Dodałem klasę ComputerPlayer (bez ciała)

---
+++ THeBoxingNights1-1/src/pl/theboxingnights/app/world/ComputerPlayer.java
@@ -0,0 +1,11 @@
+package pl.theboxingnights.app.world;
+
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.math.Vector3f;
+
+/**
+ * Created by filip / 08.06.15 / 03:43
+ */
+public class ComputerPlayer {
+
+}
