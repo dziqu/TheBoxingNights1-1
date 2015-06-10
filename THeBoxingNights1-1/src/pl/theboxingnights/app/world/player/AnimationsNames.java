53c2cb1be8af82d36eb47ae7d33f244d81bba1ee
dziku <fenix_91@go2.pl>
2015-06-10 20:44:29 +0200

    Dodałem klasę AnimationNames

---
+++ THeBoxingNights1-1/src/pl/theboxingnights/app/world/player/AnimationsNames.java
@@ -0,0 +1,46 @@
+package pl.theboxingnights.app.world.player;
+
+/**
+ * Created by filip / 10.06.15 / 20:25
+ */
+public final class AnimationsNames {
+
+    private AnimationsNames() {}
+
+    public static String getStepAnimationName() {
+        return "step";
+    }
+
+    public static String getPositionAnimationName() {
+        return "position";
+    }
+
+    public static String getRightUppercutAnimationName() {
+        return "rightUppercut";
+    }
+
+    public static String getLeftUppercutAnimationName() {
+        return "leftUppercut";
+    }
+
+    public static String getGuardAnimationName() {
+        return "guard";
+    }
+
+    public static String getLeftJabAnimationName() {
+        return "leftJab";
+    }
+
+    public static String getRightJabAnimationName() {
+        return "rightJab";
+    }
+
+    public static String getLeftHookAnimationName() {
+        return "leftHook";
+    }
+
+    public static String getRightHookAnimationName() {
+        return "rightHook";
+    }
+
+}
