c9a39e30e2a6dc3e347ebb84e708671b1d096b1d
dziku <fenix_91@go2.pl>
2015-06-10 10:31:44 +0200

    Wrzuciłem klasę PlayerControl

---
+++ THeBoxingNights1-1/src/pl/theboxingnights/app/world/player/PlayerControl.java
@@ -0,0 +1,21 @@
+package pl.theboxingnights.app.world.player;
+
+import pl.theboxingnights.app.world.UserPlayer;
+
+/**
+ * Created by filip / 10.06.15 / 10:27
+ */
+public class PlayerControl {
+
+    private UserPlayer userPlayer;
+    private int controlNumber;
+
+    public PlayerControl() {
+
+    }
+
+    public void up() {
+
+    }
+
+}
