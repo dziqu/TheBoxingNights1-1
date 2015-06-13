4de8601c7a1e56ec26ca77d10179b84b4f5458f0
dziku <fenix_91@go2.pl>
2015-06-13 17:39:34 +0200

    Stworzyłem klasę Math

---
+++ THeBoxingNights1-1/src/pl/theboxingnights/app/other/Math.java
@@ -0,0 +1,12 @@
+package pl.theboxingnights.app.other;
+
+/**
+ * Created by filip / 13.06.15 / 17:37
+ */
+public abstract class Math {
+
+    public static int add() {
+        return 0;
+    }
+
+}
